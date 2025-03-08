86187ecd3a46876ddf087ba4e58d32c255964fac
NidhiAnandan12 <x23286873@student.ncirl.ie>
2025-03-08 21:12:35 +0000

    form css changes

--- doctor_project/doctor_app/forms.py
+++ doctor_project/doctor_app/forms.py
@@ -5,18 +5,37 @@ from .models import Patient,DoctorDepartment,Doctor,Appointment
 class AppUserRegisterForm(UserCreationForm):
     class Meta:
         model = Patient
-        fields = UserCreationForm.Meta.fields + ('email','patient_address','patient_dob','patient_image')  
+        fields = UserCreationForm.Meta.fields + ('email','patient_address','patient_dob','patient_image')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'patient_address': forms.Textarea(attrs={'class': 'form-control'}),
+            'patient_dob': forms.DateInput(attrs={'class': 'form-control', 'type': "date"}),
+            'patient_image': forms.FileInput(attrs={'class': 'form-control'}),
+        }  
 
 
 class CreateDoctorForm(forms.ModelForm):
     class Meta:
         model = Doctor
         fields = ('__all__')
+        widgets = {
+           'doctor_name': forms.TextInput(attrs={'class': 'form-control'}),
+           'doctor_age': forms.NumberInput(attrs={'class': 'form-control'}),    
+              'doctor_department': forms.Select(attrs={'class': 'form-control'}),
+                'doctor_image': forms.FileInput(attrs={'class': 'form-control'}),
+                'doctor_cost': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
 class CreateDepartmentForm(forms.ModelForm):
     class Meta:
         model = DoctorDepartment
         fields = ('__all__')
+        widgets = {
+            'department_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class CreateAppoinmentForm(forms.ModelForm):
     class Meta:
